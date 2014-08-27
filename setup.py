83772cac388eaad62d4c21b20612b41de0814b0c
Thomas Fillon <thomas@parisson.com>
2014-08-28 00:56:24 +0200

    Try fixing setup.py for conda environment - step 2

--- setup.py
+++ setup.py
@@ -6,7 +6,29 @@ from setuptools.command.install import install
 
 
 def _post_install():
-    pass
+
+    import os
+    import sys
+    from distutils.sysconfig import get_python_lib
+
+    dest_dir = get_python_lib()
+
+    packages = ['gobject', 'glib', 'pygst', 'pygst.pyc', 'pygst.pth',
+                'gst-0.10']
+
+    python_version = sys.version[:3]
+    global_path = os.path.join('/usr/lib', 'python' + python_version)
+    global_sitepackages = [os.path.join(global_path,
+                                        'dist-packages'),  # for Debian-based
+                           os.path.join(global_path,
+                                        'site-packages')]  # for others
+
+    for package in packages:
+        for pack_dir in global_sitepackages:
+            src = os.path.join(pack_dir, package)
+            dest = os.path.join(dest_dir, package)
+            if not os.path.exists(dest) and os.path.exists(src):
+                os.symlink(src, dest)
 
 
 class ts_install(install):
