3125a3086e5adbca1ae7e96b532624ce36d024c9
Thomas Fillon <thomas@parisson.com>
2014-08-27 20:49:40 +0200

    Try fixing setup.py for conda environment - step 1

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-import sys
+from setuptools import setup
+from setuptools.command.install import install
+
+
+def _post_install():
+    pass
+
+
+class ts_install(install):
+    def run(self):
+        install.run(self)
+
+        self.execute(_post_install,
+                     msg="Running post install task")
 
 
 CLASSIFIERS = [
@@ -23,14 +35,14 @@ CLASSIFIERS = [
 KEYWORDS = 'audio analysis features extraction MIR transcoding graph visualize plot HTML5 interactive metadata player'
 
 setup(
-  name = "TimeSide",
-  url='https://github.com/yomguy/TimeSide/',
-  description = "open web audio processing framework",
-  long_description = open('README.rst').read(),
-  author = "Guillaume Pellerin, Paul Brossier, Thomas Fillon, Riccardo Zaccarelli, Olivier Guilyardi",
-  author_email = "yomguy@parisson.com, piem@piem.org, thomas@parisson.com, riccardo.zaccarelli@gmail.com, olivier@samalyse.com",
-  version = '0.5.7',
-  install_requires = [
+    name = "TimeSide",
+    url='https://github.com/yomguy/TimeSide/',
+    description="open web audio processing framework",
+    long_description=open('README.rst').read(),
+    author="Guillaume Pellerin, Paul Brossier, Thomas Fillon, Riccardo Zaccarelli, Olivier Guilyardi",
+    author_email="yomguy@parisson.com, piem@piem.org, thomas@parisson.com, riccardo.zaccarelli@gmail.com, olivier@samalyse.com",
+    version='0.5.7',
+    install_requires=[
         'numpy',
         'mutagen',
         'pillow',
@@ -46,13 +58,13 @@ setup(
         'djangorestframework',
         'south',
         ],
-  platforms=['OS Independent'],
-  license='Gnu Public License V2',
-  classifiers = CLASSIFIERS,
-  keywords = KEYWORDS,
-  packages = ['timeside'],
-  include_package_data = True,
-  zip_safe = False,
-  scripts=['scripts/timeside-waveforms', 'scripts/timeside-launch'],
-  #test_suite = "tests",
-    )
+    platforms=['OS Independent'],
+    license='Gnu Public License V2',
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    packages=['timeside'],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['scripts/timeside-waveforms', 'scripts/timeside-launch'],
+    cmdclass={'install': ts_install},  # override install
+)
