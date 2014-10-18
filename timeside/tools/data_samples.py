2f3bfa35f0bfe9341786e9daef433435bffbfe50
Thomas Fillon <thomas@parisson.com>
2014-10-18 16:50:01 +0200

    Try to fix Travis CI - change tools.data_samples -> need-data

--- timeside/tools/data_samples.py
+++ timeside/tools/data_samples.py
@@ -59,7 +59,7 @@ class NumpySrc:
             avalaible_sample = self.length - self.pos
             if avalaible_sample < length:
                 length = avalaible_sample
-            buf = gst.Buffer(self.array[self.pos:self.pos+length, :].data)
+            buf = gst.Buffer(self.array[self.pos:self.pos+length].data)
 
             buf.timestamp = self.pos * self.per_sample
             buf.duration = int(length*self.per_sample)
