c999cee735c6aa900ed16d472ae163f365fbaa9a
Thomas Fillon <thomas@parisson.com>
2014-12-17 08:53:16 +0100

    Split diadems grapher render_analyzers. Add a diadems subdirectory

--- timeside/grapher/render_analyzers.py
+++ timeside/grapher/render_analyzers.py
@@ -127,9 +127,9 @@ class DisplayAnalyzer(Grapher):
 
         return NewGrapher
 
-#-------------------------------------------------
+# -------------------------------------------------
 # From here define new Graphers based on Analyzers
-#-------------------------------------------------
+# -------------------------------------------------
 
 # Aubio Pitch
 try:  # because of the dependencies on the Aubio librairy
@@ -157,66 +157,3 @@ DisplayWaveform = DisplayAnalyzer.create(analyzer=wav,
                                          result_id='waveform_analyzer',
                                          grapher_id='grapher_waveform',
                                          grapher_name='Waveform from Analyzer')
-
-# IRIT 4Hz
-irit4hz = get_processor('irit_speech_4hz')
-Display4hzSpeechSegmentation = DisplayAnalyzer.create(
-    analyzer=irit4hz,
-    result_id='irit_speech_4hz.segments',
-    grapher_id='grapher_irit_speech_4hz_segments',
-    grapher_name='Speech segmentation',
-    background='waveform')
-
-
-# IRIT 4Hz with median filter
-irit4hz = get_processor('irit_speech_4hz')
-Display4hzSpeechSegmentation = DisplayAnalyzer.create(
-    analyzer=irit4hz,
-    result_id='irit_speech_4hz.segments_median',
-    grapher_id='grapher_irit_speech_4hz_segments_median',
-    grapher_name='Speech segmentation (median)',
-    background='waveform')
-
-# IRIT Monopoly
-try:  # because of the dependencies on Aubio Pitch
-    iritmonopoly = get_processor('irit_monopoly')
-    DisplayMonopoly = DisplayAnalyzer.create(
-        analyzer=iritmonopoly,
-        result_id='irit_monopoly.segments',
-        grapher_id='grapher_monopoly_segments',
-        grapher_name='Mono/Poly segmentation',
-        background='waveform')
-except PIDError:
-    pass
-
-# Limsi SAD : 2 models
-try:
-    limsi_sad = get_processor('limsi_sad')
-
-    DisplayLIMSI_SAD_etape = DisplayAnalyzer.create(
-        analyzer=limsi_sad,
-        analyzer_parameters={'sad_model': 'etape'},
-        result_id='limsi_sad.sad_lhh_diff',
-        grapher_id='grapher_limsi_sad_etape',
-        grapher_name='Speech activity (ETAPE)',
-        background='waveform')
-
-    DisplayLIMSI_SAD_maya = DisplayAnalyzer.create(
-        analyzer=limsi_sad,
-        analyzer_parameters={'sad_model': 'maya'},
-        result_id='limsi_sad.sad_lhh_diff',
-        grapher_id='grapher_limsi_sad_maya',
-        grapher_name='Speech activity (Mayan)',
-        background='waveform')
-
-except PIDError:
-    pass
-
-# IRIT Start Seg
-irit_startseg = get_processor('irit_startseg')
-DisplayIRIT_Start = DisplayAnalyzer.create(
-    analyzer=irit_startseg,
-    result_id='irit_startseg.segments',
-    grapher_id='grapher_irit_startseg',
-    grapher_name='Analogous start point',
-    background='waveform')
